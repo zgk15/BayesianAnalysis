8e55e8e605f597413897a6a9bccc862f23a96a89
zgk15 <zoltan.kiss15@imperial.ac.uk>
2018-01-19 18:05:26 +0000

    N=50 -> N=1000

--- linear1.py
+++ linear1.py
@@ -10,31 +10,31 @@ import random as rnd
 
 linear_code = """
 data {
-    int<lower=0> N;
-    vector[N] x;
-    vector[N] y;
+    int<lower=0> N; //Number of samples
+    vector[N] x; //x values
+    vector[N] y; //y values
 }
 parameters {
-    real alpha;
-    real beta;
-    real sigma;
+    real alpha; //y intercept
+    real beta; //slope
+    real sigma; //noise
 }
 model {
-    y ~ normal(alpha + beta * x, sigma);
+    y ~ normal(alpha + beta * x, sigma); //linear function equation
 }
 """
 
-N = 50
+N = 1000
 xs = []
 ys = []
 for i in range(N):
     xs.append(i)
-    ys.append((2*i+rnd.gauss(0,0.1)+2))
+    ys.append((2*i+rnd.gauss(0,0.1)+2)) # 2*x + 2 + noise
 
 
 linear_dat = {'N': N,
-               'x': xs,
-               'y': ys}
+              'x': xs,
+              'y': ys}
 
 sm = pystan.StanModel(model_code=linear_code)
 fit = sm.sampling(data=linear_dat, iter=1000, chains=4, n_jobs=1)
